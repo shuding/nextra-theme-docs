0162feaff30da0bffcf48aedfda3593e20dea802
Shu Ding <g@shud.in>
2020-09-06 22:21:43 +0800

    init

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: ['./*.js'],
+  theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
+    fontFamily: {
+      display: ['inter', 'sans-serif'],
+    },
+    letterSpacing: {
+      tight: '-0.015em'
+    }
+  }
+}
